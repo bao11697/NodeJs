b75eeb27da75e7c2603699b1aa25bf6bc973ad3b
Bao.Le <chibao11697@gmail.com>
2023-05-24 22:52:59 +0700

    Update Router , app/controller folder

--- src/index.js
+++ src/index.js
@@ -6,10 +6,11 @@ const hbs = exphbs.create({defaultLayout: 'main', extname: '.hbs'});
 const app = express()
 const port = 3000
 
+const route = require('./routers')
 app.use(express.static(path.join(__dirname,'public')))
 
 //HTTP Logger
-app.use(morgan('combined'))
+// app.use(morgan('combined'))
 
 
 //Template Engine
@@ -17,15 +18,16 @@ app.engine('hbs', hbs.engine);
 app.set('view engine', 'hbs')
 app.set('views', path.join(__dirname,'resources/views'))
 
+//Sử dụng midleware để dọc được body . 
+// Form
+app.use(express.urlencoded({
+  extended: true
+}))
+//Code JS
+app.use(express.json())
 
-
-app.get('/', (req, res) => {
-  res.render('home')
-})
-
-app.get('/news', (req, res) => {
-  res.render('news')
-})
+// Router init
+route(app)
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
