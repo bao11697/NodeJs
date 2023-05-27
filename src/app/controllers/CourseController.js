c7a93581aae90370bb4a06db188872ab56f0f181
Bao.Le <chibao11697@gmail.com>
2023-05-27 16:30:57 +0700

    Update Method Delete Course with box confirm and redirect back after deleleted

--- src/app/controllers/CourseController.js
+++ src/app/controllers/CourseController.js
@@ -45,6 +45,13 @@ class CourseController {
         .catch(next)
             
     } 
+
+    //DELETE course/:id
+    delete(req,res,next){
+        Course.deleteOne({_id : req.params.id})
+            .then(()=>res.redirect('back'))
+            .catch(next)     
+    } 
 }
 
 module.exports = new CourseController();
